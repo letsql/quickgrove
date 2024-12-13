2057afe2407a362035e0ffed178ac3de4abcd1a4
hussainsultan <hussainz@gmail.com>
2024-12-13 12:23:06 -0500

    refactor: remove auto pruning

--- src/predicates.rs
+++ src/predicates.rs
@@ -1,9 +1,4 @@
-use arrow::array::{Array, Float64Array};
-use arrow::compute::{max, min};
-use arrow::error::ArrowError;
-use arrow::record_batch::RecordBatch;
 use std::collections::HashMap;
-use std::sync::Arc;
 
 #[derive(Debug, Clone)]
 pub enum Condition {
@@ -34,41 +29,5 @@ impl Predicate {
 impl Default for Predicate {
     fn default() -> Self {
         Predicate::new()
-    }
-}
-
-pub struct AutoPredicate {
-    feature_names: Arc<Vec<String>>,
-}
-
-impl AutoPredicate {
-    pub fn new(feature_names: Arc<Vec<String>>) -> Self {
-        AutoPredicate { feature_names }
-    }
-
-    pub fn generate_predicate(&self, batch: &RecordBatch) -> Result<Predicate, ArrowError> {
-        let mut predicate = Predicate::new();
-
-        for feature_name in self.feature_names.iter() {
-            if let Some(column) = batch.column_by_name(feature_name) {
-                if let Some(float_array) = column.as_any().downcast_ref::<Float64Array>() {
-                    let min_val = min(float_array);
-                    let max_val = max(float_array);
-
-                    if let (Some(min_val), Some(max_val)) = (min_val, max_val) {
-                        predicate.add_condition(
-                            feature_name.clone(),
-                            Condition::GreaterThanOrEqual(min_val),
-                        );
-                        predicate.add_condition(
-                            feature_name.clone(),
-                            Condition::LessThan(max_val + f64::EPSILON),
-                        );
-                    }
-                }
-            }
-        }
-
-        Ok(predicate)
     }
 }
